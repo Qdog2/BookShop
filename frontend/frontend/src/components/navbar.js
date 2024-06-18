49a18a85f0b9883940da3f020a88326beda5b1ba
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-06-18 11:05:24 -0500

    navbar with css and flex with window

--- frontend/frontend/src/components/navbar.js
+++ frontend/frontend/src/components/navbar.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import "../navbar.css";
 
 function Navbar() {
+      const [active, setActive] = useState("nav__menu");
+      const [icon, setIcon] = useState("nav__toggler");
+      const navToggle = () => {
+        if (active === "nav__menu") {
+          setActive("nav__menu nav__active");
+        } else setActive("nav__menu");
+
+        if (icon === "nav__toggler") {
+          setIcon("nav__toggler toggle");
+        } else setIcon("nav__toggler");
+      };
 
     return (
         <nav className="nav">
-            <a href="#" className="brand">Books</a>
-            <ul className="nav__menu">
+            <a href="#" className="nav__brand">BookStore</a>
+            <ul className={active}>
                 <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
                 <li className="nav__item"><a href="#" className="nav__link">About</a></li>
                 <li className="nav__item"><a href="#" className="nav__link">Books</a></li>
@@ -13,7 +25,7 @@ function Navbar() {
                 <li className="nav__item"><a href="#" className="nav__link">Login</a></li>
                 <li className="nav__item"><a href="#" className="nav__link">Logout</a></li>
             </ul>
-            <div className="nav__toggler">
+            <div onClick={navToggle} className={icon}>
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
