d8eb03818c46cb639d288d000064a1c4b726b864
Quincy Nguyen <adambeckar123@gmail.com>
2024-07-18 23:05:51 -0500

    .

--- backend/springJwt/src/main/java/com/example/springJwt/model/DemoUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.springJwt.model;
-
-public class DemoUser {
-    public void demo() {
-        User user = new User();
-        user.firstName = "my first name";
-        user.password = "some password";
-    }
-}
