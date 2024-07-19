71eb4e957c8cfd74840476a461318531f9a0b2c8
Quincy Nguyen <adambeckar123@gmail.com>
2024-07-18 23:11:55 -0500

    .

---
+++ backend/springJwt/src/main/java/com/example/springJwt/service/UserDetailsImp.java
@@ -0,0 +1,12 @@
+package com.example.springJwt.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsImp implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
