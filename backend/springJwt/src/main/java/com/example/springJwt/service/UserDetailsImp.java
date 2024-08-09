4af69101b9554f4e40664fa770ef948a29daa242
Quincy Nguyen <adambeckar123@gmail.com>
2024-08-08 22:26:04 -0500

    .

--- backend/springJwt/src/main/java/com/example/springJwt/service/UserDetailsImp.java
+++ backend/springJwt/src/main/java/com/example/springJwt/service/UserDetailsImp.java
@@ -4,9 +4,10 @@ import com.example.springJwt.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsImp implements UserDetailsService {
-    @Override
 
     private final UserRepository repository;
 
