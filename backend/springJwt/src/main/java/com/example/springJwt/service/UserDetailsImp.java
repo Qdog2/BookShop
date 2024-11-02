b20b723e812b46108838e2fc376f14818016c178
Quincy Nguyen <adambeckar123@gmail.com>
2024-11-01 19:31:38 -0500

    .

--- backend/springJwt/src/main/java/com/example/springJwt/service/UserDetailsImp.java
+++ backend/springJwt/src/main/java/com/example/springJwt/service/UserDetailsImp.java
@@ -19,6 +19,8 @@ public class UserDetailsImp implements UserDetailsService {
 
     public UserRepository pause;
 
+    public UserRepository uhd;
+
     public UserDetailsImp(UserRepository repository) {
         this.repository = repository;
     }
